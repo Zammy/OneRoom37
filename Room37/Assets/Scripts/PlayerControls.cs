6a823a8f8c4f2ff82bb7d9a2022cf389d498d8b1
bgnocturnal <bgnocturnal@gmail.com>
2016-12-10 23:26:18 +0200

    Initial player movement.

---
+++ Room37/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+    private PlayerMovement player;
+
+    [SerializeField]
+    private int playerNumber;
+
+    [SerializeField]
+    private Vector2 analogueInput = Vector3.zero;
+    
+    void Awake () {
+        player = GetComponent<PlayerMovement>();
+    }
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        analogueInput.x = Input.GetAxis("Horizontal");
+        analogueInput.y = Input.GetAxis("Vertical");
+        if (analogueInput.magnitude > 0f) {
+            player.MoveRequest(analogueInput);
+        }
+    }
+}
---
+++ Room37/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Transform visual;
+    private Rigidbody2D rigidBody;
+
+    public float minWalkingSpeed;
+    public float maxWalkingSpeed;
+
+    public float WalkingSpeedRange { get; private set; }
+
+    void Awake() {
+        visual = transform.Find("Visual").transform;
+        rigidBody = transform.Find("Visual").GetComponent<Rigidbody2D>();
+
+        WalkingSpeedRange = maxWalkingSpeed - minWalkingSpeed;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveRequest(Vector2 analogueInput) {
+        float rotAngle = Mathf.Atan2(analogueInput.y, analogueInput.x) * Mathf.Rad2Deg;
+        rigidBody.MoveRotation(rotAngle);
+        rigidBody.velocity = analogueInput.normalized * (minWalkingSpeed + WalkingSpeedRange * analogueInput.magnitude);
+    }
+}
