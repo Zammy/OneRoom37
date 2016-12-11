c92daddd64c1ddf8e89225a6fe915f8aa4c4a9aa
Evgeni Petrov <evgeni.i.petrov@gmail.com>
2016-12-11 17:24:01 +0200

    Spawning of players works.

---
+++ Room37/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+    [SerializeField]
+    Transform[] startingPositions;
+
+    [SerializeField]
+    GameObject playerPrefab;
+
+	void Start () 
+    {
+        for (int i = 0; i < PlayerInfo.PlayerInfos.Length; i++)
+        {
+            var playerInfo = PlayerInfo.PlayerInfos[i];
+            var playerGo = (GameObject) Instantiate(playerPrefab);
+            playerGo.transform.localScale = Vector3.one;
+            playerGo.transform.position = startingPositions[i].transform.position;
+
+            var visual = playerGo.GetComponentInChildren<PlayerVisual>();
+            visual.SetCharacter(playerInfo.ChracterType);
+
+            var playerControls = playerGo.GetComponent<PlayerControls>();
+            playerControls.PlayerNumber = playerInfo.PlayerIndex;
+        }
+
+	}
+
+}
---
+++ Room37/Assets/Scripts/PlayerVisual.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVisual : MonoBehaviour 
+{
+    [SerializeField]
+    SpriteRenderer sprite;
+
+    [SerializeField]
+    Sprite[] characterSprites;
+
+    public void SetCharacter(CharacterType character)
+    {
+        sprite.sprite = characterSprites[(int) character];
+    }
+}
