ad3f5e8450ffe22adb9131e21b43ddfffa862f3d
Evgeni Petrov <evgeni.i.petrov@gmail.com>
2016-12-11 17:05:09 +0200

    Player info

---
+++ Room37/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CharacterType
+{
+    Detective,
+    Mike,
+    Ninja,
+    Kristoff
+}
+
+public class PlayerInfo
+{
+    public static PlayerInfo[] PlayerInfos;
+
+    public CharacterType ChracterType
+    {
+        get;
+        set;
+    }
+
+    public int PlayerIndex
+    {
+        get;
+        set;
+    }
+}
