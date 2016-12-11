87a43389b40c4fe86c971d68d7ac2268a3eeea4d
Evgeni Petrov <evgeni.i.petrov@gmail.com>
2016-12-11 14:44:12 +0200

    Player input extended.

--- Room37/Assets/Scripts/PlayerControls.cs
+++ Room37/Assets/Scripts/PlayerControls.cs
@@ -1,6 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+public enum InputButton
+{
+    Interact, Motive, Opportunity, Means
+}
 
 public class PlayerControls : MonoBehaviour 
 {
@@ -13,6 +19,27 @@ public class PlayerControls : MonoBehaviour
     [SerializeField]
     private Vector2 analogueInput = Vector3.zero;
 
+    Dictionary<InputButton, string> BUTTONS_TO_AXIS;
+    Dictionary<InputButton, bool> buttonPressedState = new Dictionary<InputButton, bool>()
+    {
+        {InputButton.Interact, false},
+        {InputButton.Motive, false},
+        {InputButton.Opportunity, false},
+        {InputButton.Means, false},
+    };
+
+    public event Action<InputButton> ButtonPressed;
+
+    void Awake()
+    {
+        BUTTONS_TO_AXIS = new Dictionary<InputButton, string>();
+        var inputButtons = (InputButton[]) Enum.GetValues(typeof(InputButton));
+        foreach (var inputButton in inputButtons)
+        {
+            BUTTONS_TO_AXIS[inputButton] = inputButton.ToString();
+        }
+    }
+
 
     // Update is called once per frame
     void Update() 
@@ -20,5 +47,27 @@ public class PlayerControls : MonoBehaviour
         analogueInput.x = Input.GetAxis("Horizontal");
         analogueInput.y = Input.GetAxis("Vertical");
         player.MoveRequest(analogueInput);
+
+
+        foreach(var kvp in BUTTONS_TO_AXIS)
+        {
+            InputButton inputButton = kvp.Key;
+            string axisName = kvp.Value;
+            bool isPressed = Input.GetAxisRaw(axisName) > 0;
+            bool wasPressed = buttonPressedState[inputButton];
+            if (isPressed && !wasPressed)
+            {
+                RaiseButtonPressed(inputButton);
+            }
+            buttonPressedState[inputButton] = isPressed;
+        }
+    }
+
+    void RaiseButtonPressed(InputButton inputButton)
+    {
+        if (this.ButtonPressed != null)
+        {
+            this.ButtonPressed(inputButton);
+        }
     }
 }
